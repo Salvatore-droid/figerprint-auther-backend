ce72c578546f4f2fce34ca99676d6f8f2428afea
Genius <149167886+Salvatore-droid@users.noreply.github.com>
2025-03-22 13:34:37 +0300

    Update server.js to allow production origin

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const CLIENT_URL = "http://localhost:5173";
+const CLIENT_URL = [
+  "http://localhost:5173",
+  "https://fingerprint-auther-frontend.onrender.com"
+];
 const RP_ID = "localhost";
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
